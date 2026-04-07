2ed3500c7fa849f284fc4db069de651f89b301f2
DeepKansara1212 <deepkansara1212@gmail.com>
2026-04-07 10:00:50 +0530

    Add JS Basics

---
+++ JAVASCRIPT/01_Basics/07_Operators.js
@@ -0,0 +1,73 @@
+// Arithmetic Operators = +, -, *, /, %, **
+
+let a = 10
+let b = 4
+
+let c = a * b;
+// console.log(c); 
+
+let d = 50 + 20 + "10" + 20
+// console.log(typeof d);
+
+let e = 'abc' - '2'
+// console.log(e);
+
+
+let eq = 10 + 45 - 50 * 2 / 4 % 3
+// console.log(eq);
+
+
+// 10 + 45 - 100 / 4 % 3
+// 10 + 45 - 25 % 3
+// 10 + 45 - 1
+// 55 - 1
+// 54
+
+
+// Precedence Order & Associativity Rule
+// ()
+// /, *, %     -> Left to Right
+// +, -        -> Left to Right
+
+
+
+
+// Assignment Operator -> =, +=, -=, *=
+
+let age = 20
+// let num1 = num2 = num3 = 50
+
+// num1 *= 3
+
+age += 5
+// age = age + 5
+
+
+
+
+
+// Inc / Dec
+let counter = 5
+// console.log(counter);
+
+let q = counter++
+// console.log(q);
+let r = ++counter
+// console.log(r);
+
+
+
+// Comparison Operator -> >, >=, <, <=, ==, ===, !=, !==
+let num2 = 20;
+let num3 = "20";
+
+// == -> Values must be same 
+// != -> Values not be same
+// ===  -> Values & Datatypes must be same
+// !==  -> Values & Datatypes not be same
+
+console.log(num2 != num3);
+
+
+console.log(typeof null);    // Object
+
