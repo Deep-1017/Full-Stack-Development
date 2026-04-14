79ff7f75712721e284468df8a339c4b1d79d4201
DeepKansara1212 <deepkansara1212@gmail.com>
2026-04-14 10:41:24 +0530

    Add JS Conditionals

---
+++ JAVASCRIPT/02_Conditionals/05_Switch-case.js
@@ -0,0 +1,22 @@
+let a = 2 + 4;
+
+switch (a) {
+  case 1:
+    console.log("Too Small");
+    break;
+  case 2:
+    console.log("Small");
+    break;
+  case 3:
+    console.log("Almost");
+    break;
+  case 4:
+    console.log("Exact");
+    break;
+  case 5:
+    console.log("Bigger");
+    break;
+  default:
+    console.log("Not Valid");
+    break;
+}
