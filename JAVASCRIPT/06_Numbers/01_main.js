74d1733cd9db7bea0bc1f5252d5e81f5238458f5
DeepKansara1212 <deepkansara1212@gmail.com>
2026-05-02 12:21:27 +0530

    Add JS Numbers

---
+++ JAVASCRIPT/06_Numbers/01_main.js
@@ -0,0 +1,9 @@
+let num = 10
+console.log(typeof(num));
+
+
+const number = new Number(20) 
+console.log(typeof(number));
+
+console.log(num == number);
+console.log(num === number);
---
+++ JAVASCRIPT/06_Numbers/02_Methods.js
@@ -0,0 +1,25 @@
+let x = 9.656;
+// console.log(x.toFixed(0));
+// console.log(x.toFixed(2));
+// console.log(x.toFixed(4));
+// console.log(x.toFixed(6));
+
+
+let y = 9.656;
+// console.log(y.toPrecision());
+// console.log(y.toPrecision(2));
+// console.log(y.toPrecision(4));
+// console.log(y.toPrecision(6));
+
+// console.log(y.valueOf());
+
+
+console.log(parseFloat("10"));
+
+
+
+// console.log(x);
+
+
+// toString() method
+// console.log(x.toString(2));
