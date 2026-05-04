7ffc4ba479709cc8b52afe163a0c025e3cd3cbb7
DeepKansara1212 <deepkansara1212@gmail.com>
2026-05-04 11:29:03 +0530

    Add JS Strings

---
+++ JAVASCRIPT/07_Strings/01_main.js
@@ -0,0 +1,52 @@
+let str1 = "Hi! My name is Javascript.";
+let str2 = "\nI am a Programming Language!";
+
+// console.log(str.length);
+
+// console.log(str.charAt(-10));
+// console.log(str.charCodeAt(10));
+// console.log(str.at(-10));
+// console.log(str[10]);
+
+// let str3 = str1.concat(str2);
+// console.log(str3);
+// let str4 = str1 + str2
+// console.log(str4);
+
+// let sliceStr = str1.slice(2, 8);
+// let sliceStr = str1.substring(2, 8);
+// let sliceStr = str1.substr(2, 8);
+// console.log(sliceStr);
+
+// let str3 = "Hello"
+// console.log(str3.toUpperCase());
+// console.log(str3.toLowerCase());
+
+// let text = "5"
+// let padText = text.padStart(5, "$")
+// let padText = text.padEnd(5, "$")
+// console.log(padText);
+
+// let text = "Hello world!\n";
+// let result = text.repeat(4);
+// console.log(result);
+
+// let text = "Please visit Microsoft! Microsoft is a Product based company.";
+// let newText = text.replaceAll("Microsoft", "W3Schools");
+// console.log(newText);
+
+// let text = "Hello, Javascript!"
+// let newText = text.split(",")
+// console.log(newText);
+
+// let text = "Please locate, locate, locate where 'locate' occurs!";
+// let index = text.search("locate");
+// console.log(index);
+
+// let text = "The rain in SPAIN stays mainly in the plain";
+// let newText = text.match(/ain/gi);
+// console.log(newText);
+
+// let text = "Hello world, welcome to the universe.";
+// let newText = text.includes("world", 12);
+// console.log(newText);
