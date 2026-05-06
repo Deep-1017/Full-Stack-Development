ac11b7efe70abb7a325be634592f96838c0f3be6
DeepKansara1212 <deepkansara1212@gmail.com>
2026-05-06 10:51:42 +0530

    Add Array Methods

--- JAVASCRIPT/08_Array/03_Arr-Methods.js
+++ JAVASCRIPT/08_Array/03_Arr-Methods.js
@@ -1,4 +1,20 @@
-const fruits = ["apple", "mango", "banana", "grapes"];
+const fruits = ["apple", "mango", "banana", "grapes", "kiwi", "orange", "litchi", "cherry"];
+
+
+console.log(fruits.copyWithin(2, 0, 3));
+
+
+// const cars = ["BMW", "Audi", "Thar"]
+
+// console.log(fruits.toString())
+
+// console.log(fruits.join(" $ "));
+
+// console.log(fruits.concat(cars));
+
+
+
+
 
 // console.log(fruits[fruits.length - 1]);
 // console.log(fruits.at(-2));
@@ -8,8 +24,8 @@ const fruits = ["apple", "mango", "banana", "grapes"];
 // fruits.pop()
 // fruits.pop()
 
-fruits.shift()
-fruits.unshift("Kiwi")
+// fruits.shift()
+// fruits.unshift("Kiwi")
 
-console.log(fruits);
+// console.log(fruits);
 
---
+++ JAVASCRIPT/08_Array/04_Arr-Loops.js
@@ -0,0 +1,21 @@
+// const fruits = ["apple", "mango", "banana", "grapes"];
+
+// for (const fruit of fruits) {
+//     console.log(fruit);
+// }
+
+// console.log(fruits.length);         // 4
+
+// fruits.length = 3
+
+// console.log(fruits);
+
+// fruits.length = 7
+// console.log(fruits);
+
+
+const users = new Array("John", "alice", "jack", "ann")
+const cars = new Array(5)
+
+console.log(users);
+console.log(cars);
