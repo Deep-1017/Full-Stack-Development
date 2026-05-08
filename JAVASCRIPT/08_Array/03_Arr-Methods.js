db77820b5f8914ee1128d16a2ecbccf2b56ab433
DeepKansara1212 <deepkansara1212@gmail.com>
2026-05-08 10:32:31 +0530

    Add JS Array Methods

--- JAVASCRIPT/08_Array/03_Arr-Methods.js
+++ JAVASCRIPT/08_Array/03_Arr-Methods.js
@@ -1,8 +1,35 @@
-const fruits = ["apple", "mango", "banana", "grapes", "kiwi", "orange", "litchi", "cherry"];
+const fruits = [
+  "apple",
+  "mango",
+  "banana",
+  "grapes",
+  "kiwi",
+  "orange",
+  "litchi",
+  "cherry",
+];
 
+// const updatedFruits = fruits.splice(2, 5, "pineapple", "watermelon")
+// console.log(updatedFruits);
+// console.log(fruits);
 
-console.log(fruits.copyWithin(2, 0, 3));
+// console.log(fruits.slice(1));
 
+// function findNumber(value, index, array) {
+//   return value > 18;
+// }
+
+const numbers = [4, 9, 16, 25, 29];
+const firstFind = numbers.findLast((value, index, array) => {
+  return value > 18;
+});
+console.log(firstFind);
+
+// console.log(fruits.copyWithin(2, 0));
+
+// const myArr = [[1,2],[3,4],[5,6]];
+// const newArr = myArr.flat();
+// console.log(newArr);
 
 // const cars = ["BMW", "Audi", "Thar"]
 
@@ -11,10 +38,6 @@ console.log(fruits.copyWithin(2, 0, 3));
 // console.log(fruits.join(" $ "));
 
 // console.log(fruits.concat(cars));
-
-
-
-
 
 // console.log(fruits[fruits.length - 1]);
 // console.log(fruits.at(-2));
@@ -28,4 +51,3 @@ console.log(fruits.copyWithin(2, 0, 3));
 // fruits.unshift("Kiwi")
 
 // console.log(fruits);
-
