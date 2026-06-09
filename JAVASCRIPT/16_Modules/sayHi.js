6e94b132e203617061b32e6438ef093300d1c473
DeepKansara1212 <deepkansara1212@gmail.com>
2026-06-09 11:21:08 +0530

    Add JS Modules

---
+++ JAVASCRIPT/16_Modules/sayHi.js
@@ -0,0 +1,9 @@
+// Default Export
+export default function sayHi(user) {
+  console.log(`Hello, ${user}!`);
+}
+
+// Named Export
+export function add(a, b) {
+    console.log(a + b);
+}
---
+++ JAVASCRIPT/16_Modules/userGreet.js
@@ -0,0 +1,5 @@
+import sayHi   from "./sayHi.js";
+import { add as hi} from "./sayHi.js";
+
+sayHi("Deep");
+hi(10, 2)
