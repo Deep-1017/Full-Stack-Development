3281b319104af268bf56db3800c95fa5e51fc1e7
DeepKansara1212 <deepkansara1212@gmail.com>
2026-06-22 10:40:26 +0530

    Add Adv JS

--- Adv Javascript/01_Async-JS/03_setInterval.js
+++ Adv Javascript/01_Async-JS/03_setInterval.js
@@ -1,11 +1,11 @@
 function greet() {
-    console.log(`Hello`);
+  console.log(`Hello`);
 }
 
 setInterval(greet, 2000);
 
 function greet(name) {
-    console.log(`Hello ${name}`);
+  console.log(`Hello ${name}`);
 }
 
 const intervalId = setInterval(greet, 2000, "Deep");
@@ -13,3 +13,11 @@ const intervalId = setInterval(greet, 2000, "Deep");
 clearInterval(intervalId);
 
 // Logs 'Hello' every 2 seconds
+
+step1(function (r1) {
+  step2(r1, function (r2) {
+    step3(r2, function (r3) {
+      console.log(r3);
+    });
+  });
+});
