9ed5c58196f13b8746a58a91f6321536f7de22aa
DeepKansara1212 <deepkansara1212@gmail.com>
2026-06-22 10:40:38 +0530

    Add Bootstrap

---
+++ Solution/main.js
@@ -0,0 +1,33 @@
+// const Person = {
+//     firstName: "Deep",
+//     lastName: "Sharma",
+//     describe: function() {
+//         return `My name is ${this.firstName} ${this.lastName}`;
+//     }
+// }
+
+// function getFullName(person) {
+//     return `${person.firstName} ${person.lastName}`;
+// }
+
+// console.log(getFullName(Person));
+// console.log(Person.describe());
+
+
+
+const library = {
+    name: "City Library",
+    location: "Downtown",
+    books: [
+        {
+            title: "The Great Gatsby",
+            author: "F. Scott Fitzgerald",
+            available: true
+        },
+        {
+            title: "To Kill a Mockingbird",
+            author: "Harper Lee",
+            available: false
+        }
+    ]
+}
